440c1bccc7dc82215d24fce9316745df5150395f
Gue <guehyang@gmail.com>
2019-08-01 19:47:26 -0700

    Javascript 90% completed.  No input validation yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,67 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAUSzypFPRie74qYrjtie27lORr5J6MYb8",
-    authDomain: "train-schedule-600e9.firebaseapp.com",
-    databaseURL: "https://train-schedule-600e9.firebaseio.com",
-    projectId: "train-schedule-600e9",
-    storageBucket: "",
-    messagingSenderId: "990869645051",
-    appId: "1:990869645051:web:b97f2a1782c07246"
+$( document ).ready(function() {
+    // Your web app's Firebase configuration
+    const config = {
+        apiKey: "AIzaSyAUSzypFPRie74qYrjtie27lORr5J6MYb8",
+        authDomain: "train-schedule-600e9.firebaseapp.com",
+        databaseURL: "https://train-schedule-600e9.firebaseio.com",
+        projectId: "train-schedule-600e9",
+        storageBucket: "",
+        messagingSenderId: "990869645051",
+        appId: "1:990869645051:web:b97f2a1782c07246"
     };
+    // Initialize Firebase
+    firebase.initializeApp(config);
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    // Create a variable to reference the database
+    var database = firebase.database();
 
-let name = "";
-let destination = "";
-let firstTime = "";
-let frequency = "";
+    let train = {
+        "name": "",
+        "destination": "",
+        "firstTime": "",
+        "frequency": "",
+        "minsAway": "",
+        "nextArrival": ""
+    }
 
-function returnTemplateLiterals() {
-    return `
+    function minsAway() {
+        let firstTimeConverted = moment(train.firstTime, "HH:mm").subtract(1, "years");
+        let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        let tRemainder = diffTime % train.frequency;
+        let tMinutesTillTrain = train.frequency - tRemainder;
+        return tMinutesTillTrain;
+    }
 
-    `
-}
+    function nextArrival() {
+        let nextTrain = moment().add(train.minsAway, "minutes");
+        let formattedMinsAway = moment(nextTrain).format("hh:mm A");
+        return formattedMinsAway;
+    }
 
-$(".btn").on("click", function() {
-    event.preventDefault();
-    name = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTime = $("#first-time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-
-    database.ref().set({
-        trainName: name,
-        trainDestination: destination,
-        trainFirstTime: firstTime,
-        trainFrequency: frequency
+    $("#submit").on("click", function(event) {
+        event.preventDefault();
+        train.name = $("#name-input").val().trim();
+        train.destination = $("#destination-input").val().trim();
+        train.firstTime = $("#first-time-input").val().trim();
+        train.frequency = $("#frequency-input").val().trim();
+        train.minsAway = minsAway();
+        train.nextArrival = nextArrival();
+        database.ref().push({
+            train: train
+        });
     });
-});
 
-database.ref().on("value", function(snapshot) {
-    console.log(snapshot);
+    database.ref().on("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        let child = snapshot.val().train;
+        $("#table-body").append(`
+        <tr train-name="${child.name}">
+            <th scope="row">${child.name}</th>
+            <td>${child.destination}</td>
+            <td>${child.frequency}</td>
+            <td>${child.nextArrival}</td>
+            <td>${child.minsAway}</td>
+        </tr>
+        `)
+    });
 });
